fbda470f2ea45cbe3bd8abbb10a3b233ed90c427
Frederik Hölter <hoelterfrederik@web.de>
2024-11-21 20:33:34 +0100

    working for venv + crontab

--- LockKeyLoginAuto.py
+++ LockKeyLoginAuto.py
@@ -1,17 +1,17 @@
 import yaml
 import requests
+import os
 
+path = os.path.dirname(__file__)
 url = 'https://login.ruhr-uni-bochum.de/cgi-bin/laklogin'
 
 
-conf = yaml.safe_load(open('loginDetails.yaml'))
+conf = yaml.safe_load(open(path + '/' + 'loginDetails.yaml'))
 myMail = conf['lock_key_user']['email']
 myPassword = conf['lock_key_user']['password']
-ip = conf['lock_key_user']['ipadress']
 
 values = {'loginid' : myMail,
         'password' : myPassword,
-        ##'ipaddr' : ip,
         'action': 'Login'}
 
 r = requests.post(url, data=values)
